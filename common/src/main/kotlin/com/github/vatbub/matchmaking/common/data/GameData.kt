c14f52e798a4f883ca3dab2fd68137b01371e663
Frederik Kammel <vatbub123+github@googlemail.com>
2019-01-09 01:28:07 +0100

    Added docs to the GameData class and regenerated its equality members

--- common/src/main/kotlin/com/github/vatbub/matchmaking/common/data/GameData.kt
+++ common/src/main/kotlin/com/github/vatbub/matchmaking/common/data/GameData.kt
@@ -19,14 +19,95 @@
  */
 package com.github.vatbub.matchmaking.common.data
 
+/**
+ * Contains the current state of the game as defined by the game host.
+ * The data in this class can be stored and retrieved by using key-value-pairs.
+ * In theory, this class can store data of any type (including your own types), but it has been tested with the following:
+ * - [Byte]
+ * - [ByteArray]
+ * - [Char]
+ * - [CharArray]
+ * - [String]
+ * - Arrays of [String]s
+ * - [List]<[String]>
+ * - [Float]
+ * - [FloatArray]
+ * - [List]<[Integer]>
+ * - [Short]
+ * - [ShortArray]
+ */
 class GameData {
+    private val contents = mutableMapOf<String, Any>()
+
+    /**
+     * Stores the specified value together with its key.
+     * @param key The key to be used to store the data.
+     * @param content The data associated with the key.
+     */
+    operator fun <T : Any> set(key: String, content: T) {
+        contents[key] = content
+    }
+
+    /**
+     * Retrieves the value associated with the specified key.
+     *
+     * **Important: Type safety can only be guaranteed if [typeClass] is specified. If [typeClass] is not specified
+     * and the value associated with the key is not assignable to [T], a [ClassCastException] will be thrown.**
+     * @param key The key of the data to get.
+     * @param defaultValue The value to be returned if the key is not found or if the data associated with the key is
+     * not of type [T] (see the note on type safety above)
+     * @param typeClass The [Class] that represents the type of data to be returned. This must be specified in order to
+     * guarantee type safety (see the note above)
+     * @return The data associated with the key or [defaultValue] if the key was not found or the data associated with
+     * the key is not of type [T] (see the note on type safety above) or `null` if [defaultValue] is not specified.
+     */
+    operator fun <T : Any> get(key: String, defaultValue: T? = null, typeClass: Class<T>? = null): T? {
+        if (!contains(key))
+            return defaultValue
+
+        val result = contents[key]
+        if (typeClass != null && result != null && !typeClass.isAssignableFrom(result.javaClass))
+            return defaultValue
+
+        @Suppress("UNCHECKED_CAST")
+        return result as T?
+
+    }
+
+    /**
+     * Checks whether the specified key has any data associated with it.
+     * @param key The key to be looked up.
+     * @return `true` if the key has any data associated with it, `false` otherwise.
+     */
+    fun contains(key: String): Boolean {
+        return contents.containsKey(key)
+    }
+
+
+    /**
+     * Removes the specified key and its corresponding value from this map.
+     *
+     * @return the previous value associated with the key, or `null` if the key was not present in the map.
+     */
+    @Suppress("UNCHECKED_CAST")
+    fun <T : Any> remove(key: String): T? {
+        return contents.remove(key) as T?
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
+
+        other as GameData
+
+        if (contents != other.contents) return false
+
         return true
     }
 
     override fun hashCode(): Int {
-        return javaClass.hashCode()
+        return contents.hashCode()
     }
+
+
 }
