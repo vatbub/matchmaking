9f714dc1406c14277b38153e4ba4099dece26265
Frederik Kammel <vatbub123+github@googlemail.com>
2019-02-08 01:45:16 +0100

    McGyvered jsimone's webapp-runner to create a standalone server launcher

---
+++ standaloneserverlauncher/src/main/kotlin/com/github/vatbub/matchmaking/standaloneserverlauncher/CommandLineParams.kt
@@ -0,0 +1,155 @@
+/*-
+ * #%L
+ * matchmaking.standalone-server-launcher
+ * %%
+ * Copyright (C) 2016 - 2019 Frederik Kammel
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.github.vatbub.matchmaking.standaloneserverlauncher
+
+import com.beust.jcommander.DynamicParameter
+import com.beust.jcommander.Parameter
+import java.util.*
+
+
+class CommandLineParams {
+    @Parameter(
+        names = ["--session-timeout"],
+        description = "The number of minutes of inactivity before a user's session is timed out."
+    )
+    var sessionTimeout: Int? = null
+
+    @Parameter(names = ["--port"], description = "The port that the server will accept http requests on.")
+    var port: Int? = 8080
+
+    @Parameter(names = ["--context-xml"], description = "The path to the context xml to use.")
+    var contextXml: String? = null
+
+    @Parameter(names = ["--path"], description = "The context path")
+    var contextPath = ""
+
+    @Parameter(
+        names = ["--shutdown-override"],
+        description = "Overrides the default behavior and casues Tomcat to ignore lifecycle failure events rather than shutting down when they occur."
+    )
+    var shutdownOverride = false
+
+    @Parameter(names = ["--help"], help = true)
+    var help: Boolean = false
+
+    @Parameter(names = ["--enable-compression"], description = "Enable GZIP compression on responses")
+    var enableCompression: Boolean = false
+
+    @Parameter(
+        names = ["--compressable-mime-types"],
+        description = "Comma delimited list of mime types that will be compressed when using GZIP compression."
+    )
+    var compressableMimeTypes =
+        "text/html,text/xml,text/plain,text/css,application/json,application/xml,text/javascript,application/javascript"
+
+    @Parameter(
+        names = ["--enable-ssl"],
+        description = "Specify -Djavax.net.ssl.keyStore, -Djavax.net.ssl.keystoreStorePassword, -Djavax.net.ssl.trustStore and -Djavax.net.ssl.trustStorePassword in JAVA_OPTS. Note: should not be used if a reverse proxy is terminating SSL for you (such as on Heroku)"
+    )
+    var enableSSL: Boolean = false
+
+    @Parameter(
+        names = ["--enable-client-auth"],
+        description = "Specify -Djavax.net.ssl.keyStore and -Djavax.net.ssl.keyStorePassword in JAVA_OPTS"
+    )
+    var enableClientAuth: Boolean = false
+
+    @Parameter(
+        names = ["--enable-basic-auth"],
+        description = "Secure the app with basic auth. Use with --basic-auth-user and --basic-auth-pw or --tomcat-users-location"
+    )
+    var enableBasicAuth = false
+
+    @Parameter(
+        names = ["--basic-auth-user"],
+        description = "Username to be used with basic auth. Defaults to BASIC_AUTH_USER env variable."
+    )
+    var basicAuthUser: String? = null
+
+    @Parameter(
+        names = ["--basic-auth-pw"],
+        description = "Password to be used with basic auth. Defaults to BASIC_AUTH_PW env variable."
+    )
+    var basicAuthPw: String? = null
+
+    @Parameter(
+        names = ["--tomcat-users-location"],
+        description = "Location of the tomcat-users.xml file. (relative to the location of the webapp-runner jar file)"
+    )
+    var tomcatUsersLocation: String? = null
+
+    @Parameter(
+        names = ["--expanded-dir-name"],
+        description = "The name of the directory the WAR file will be expanded into."
+    )
+    var expandedDirName = "expanded"
+
+    @Parameter(names = ["--uri-encoding"], description = "Set the URI encoding to be used for the Connector.")
+    var uriEncoding: String? = null
+
+    @Parameter(
+        names = ["--use-body-encoding-for-uri"],
+        description = "Set if the entity body encoding should be used for the URI."
+    )
+    var useBodyEncodingForURI = false
+
+    @Parameter(names = ["--scanBootstrapClassPath"], description = "Set jar scanner scan bootstrap classpath.")
+    var scanBootstrapClassPath = false
+
+    @Parameter(
+        names = ["--temp-directory"],
+        description = "Define the temp directory, default value: ./target/tomcat.PORT"
+    )
+    var tempDirectory: String? = null
+
+    @Parameter(
+        names = ["--bind-on-init"],
+        description = "Controls when the socket used by the connector is bound. By default it is bound when the connector is initiated and unbound when the connector is destroyed., default value: true",
+        arity = 1
+    )
+    var bindOnInit = true
+
+    @Parameter(names = ["--proxy-base-url"], description = "Set proxy URL if tomcat is running behind reverse proxy")
+    var proxyBaseUrl = ""
+
+    @Parameter(names = ["--max-threads"], description = "Set the maximum number of worker threads")
+    var maxThreads: Int? = 0
+
+    @Parameter(
+        names = ["--memcached-transcoder-factory-class"],
+        description = "The class name of the factory that creates the transcoder to use for serializing/deserializing sessions to/from memcached."
+    )
+    var memcachedTranscoderFactoryClass: String? = null
+
+    @DynamicParameter(
+        names = ["-A"],
+        description = "Allows setting HTTP connector attributes. For example: -Acompression=on"
+    )
+    var attributes: Map<String, String> = HashMap()
+
+    @Parameter(names = ["--enable-naming"], description = "Enables JNDI naming")
+    var enableNaming = false
+
+    @Parameter(names = ["--access-log"], description = "Enables AccessLogValue to STDOUT")
+    var accessLog = false
+
+    @Parameter(names = ["--access-log-pattern"], description = "If --access-log is enabled, sets the logging pattern")
+    var accessLogPattern = "common"
+}
---
+++ standaloneserverlauncher/src/main/kotlin/com/github/vatbub/matchmaking/standaloneserverlauncher/Main.kt
@@ -0,0 +1,374 @@
+/*-
+ * #%L
+ * matchmaking.standalone-server-launcher
+ * %%
+ * Copyright (C) 2016 - 2019 Frederik Kammel
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.github.vatbub.matchmaking.standaloneserverlauncher
+
+import com.beust.jcommander.JCommander
+import org.apache.catalina.Context
+import org.apache.catalina.LifecycleException
+import org.apache.catalina.LifecycleState
+import org.apache.catalina.connector.Connector
+import org.apache.catalina.core.StandardContext
+import org.apache.catalina.core.StandardServer
+import org.apache.catalina.startup.Tomcat
+import org.apache.catalina.users.MemoryUserDatabase
+import org.apache.catalina.users.MemoryUserDatabaseFactory
+import org.apache.commons.io.IOUtils
+import org.apache.coyote.AbstractProtocol
+import org.apache.tomcat.util.descriptor.web.LoginConfig
+import org.apache.tomcat.util.descriptor.web.SecurityCollection
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint
+import org.apache.tomcat.util.scan.StandardJarScanner
+import java.io.*
+import java.net.URI
+import java.nio.file.Files
+import javax.naming.CompositeName
+import javax.naming.StringRefAddr
+import javax.servlet.annotation.ServletSecurity
+
+
+class Main {
+    companion object {
+        private const val authRole = "user"
+        private const val warName = "/serverWar.war"
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val commandLineParams = CommandLineParams()
+            @Suppress("DEPRECATION")
+            val jCommander = JCommander(commandLineParams, *args)
+
+            if (commandLineParams.help) {
+                jCommander.usage()
+                System.exit(1)
+            }
+
+            val tomcat = Tomcat()
+            tomcat.setBaseDir(resolveTomcatBaseDir(commandLineParams.port, commandLineParams.tempDirectory))
+
+            val port = commandLineParams.port
+
+            if (port == null) {
+                println("Port not specified")
+                jCommander.usage()
+                System.exit(1)
+                return
+            }
+
+            val nioConnector = Connector("org.apache.coyote.http11.Http11NioProtocol")
+            nioConnector.port = port
+
+            if (!commandLineParams.attributes.isEmpty()) {
+                println("Connector attributes")
+                for (entry in commandLineParams.attributes) {
+                    val key = entry.key
+                    val value = entry.value
+                    println("property: $key - $value")
+                    nioConnector.setProperty(entry.key, entry.value)
+                }
+            }
+
+            if (commandLineParams.enableSSL) {
+                nioConnector.secure = true
+                nioConnector.setProperty("SSLEnabled", "true")
+                nioConnector.setProperty("allowUnsafeLegacyRenegotiation", "false")
+                val pathToTrustStore = System.getProperty("javax.net.ssl.trustStore")
+                if (pathToTrustStore != null) {
+                    nioConnector.setProperty("sslProtocol", "tls")
+                    val truststoreFile = File(pathToTrustStore)
+                    nioConnector.setAttribute("truststoreFile", truststoreFile.absolutePath)
+                    println(truststoreFile.absolutePath)
+                    nioConnector.setAttribute(
+                        "trustStorePassword",
+                        System.getProperty("javax.net.ssl.trustStorePassword")
+                    )
+                }
+                val pathToKeystore = System.getProperty("javax.net.ssl.keyStore")
+                if (pathToKeystore != null) {
+                    val keystoreFile = File(pathToKeystore)
+                    nioConnector.setAttribute("keystoreFile", keystoreFile.absolutePath)
+                    println(keystoreFile.absolutePath)
+                    nioConnector.setAttribute("keystorePass", System.getProperty("javax.net.ssl.keyStorePassword"))
+                }
+                if (commandLineParams.enableClientAuth) {
+                    nioConnector.setAttribute("clientAuth", true)
+                }
+
+            }
+
+            if (commandLineParams.proxyBaseUrl.isNotEmpty()) {
+                val uri = URI(commandLineParams.proxyBaseUrl)
+                val scheme = uri.scheme
+                nioConnector.proxyName = uri.host
+                nioConnector.scheme = scheme
+                if (scheme == "https" && !nioConnector.secure) {
+                    nioConnector.secure = true
+                }
+                when {
+                    uri.port > 0 -> nioConnector.proxyPort = uri.port
+                    scheme == "http" -> nioConnector.proxyPort = 80
+                    scheme == "https" -> nioConnector.proxyPort = 443
+                }
+            }
+
+            if (null != commandLineParams.uriEncoding) {
+                nioConnector.uriEncoding = commandLineParams.uriEncoding
+            }
+            nioConnector.useBodyEncodingForURI = commandLineParams.useBodyEncodingForURI
+
+            if (commandLineParams.enableCompression) {
+                nioConnector.setProperty("compression", "on")
+                nioConnector.setProperty("compressableMimeType", commandLineParams.compressableMimeTypes)
+            }
+
+            if (!commandLineParams.bindOnInit) {
+                nioConnector.setProperty("bindOnInit", "false")
+            }
+
+            val maxThreads = commandLineParams.maxThreads
+            if (maxThreads != null && maxThreads > 0) {
+                val handler = nioConnector.protocolHandler
+                if (handler is AbstractProtocol<*>) {
+                    handler.setMaxThreads(maxThreads)
+                } else {
+                    println("WARNING: Could not set maxThreads!")
+                }
+            }
+
+            tomcat.connector = nioConnector
+
+            tomcat.setPort(port)
+
+
+            val context: Context
+
+            if (commandLineParams.contextPath.isNotEmpty() && !commandLineParams.contextPath.startsWith("/")) {
+                System.out.println("WARNING: You entered a path: [" + commandLineParams.contextPath + "]. Your path should start with a '/'. Tomcat will update this for you, but you may still experience issues.")
+            }
+
+            val contextPath = commandLineParams.contextPath
+
+            val war = exportResource(warName)
+
+            println("Adding Context " + contextPath + " for " + war.path)
+            context = tomcat.addWebapp(contextPath, war.absolutePath)
+
+            (context as StandardContext).unpackWAR = false
+
+            if (!commandLineParams.shutdownOverride) {
+                // allow Tomcat to shutdown if a context failure is detected
+                context.addLifecycleListener { event ->
+                    if (event.lifecycle.state === LifecycleState.FAILED) {
+                        val server = tomcat.server
+                        if (server is StandardServer) {
+                            System.err.println("SEVERE: Context [" + contextPath + "] failed in [" + event.lifecycle::class.java.name + "] lifecycle. Allowing Tomcat to shutdown.")
+                            server.stopAwait()
+                        }
+                    }
+                }
+            }
+
+            if (commandLineParams.scanBootstrapClassPath) {
+                val scanner = StandardJarScanner()
+                scanner.isScanBootstrapClassPath = true
+                context.setJarScanner(scanner)
+            }
+
+            if (commandLineParams.contextXml != null) {
+                System.out.println("Using context config: " + commandLineParams.contextXml)
+                context.setConfigFile(File(commandLineParams.contextXml).toURI().toURL())
+            }
+
+            //set the session timeout
+            val sessionTimeout = commandLineParams.sessionTimeout
+            if (sessionTimeout != null)
+                context.setSessionTimeout(sessionTimeout)
+
+            addShutdownHook(tomcat)
+
+            if (commandLineParams.enableNaming ||
+                commandLineParams.enableBasicAuth ||
+                commandLineParams.tomcatUsersLocation != null
+            ) {
+                tomcat.enableNaming()
+            }
+
+            if (commandLineParams.enableBasicAuth)
+                enableBasicAuth(context, commandLineParams.enableSSL)
+
+
+            if (commandLineParams.accessLog) {
+                val host = tomcat.host
+                val valve = StdoutAccessLogValve()
+                valve.enabled = true
+                valve.pattern = commandLineParams.accessLogPattern
+                host.pipeline.addValve(valve)
+            }
+
+            tomcat.start()
+
+            /*
+             * NamingContextListener.lifecycleEvent(LifecycleEvent event)
+             * cannot initialize GlobalNamingContext for Tomcat until
+             * the Lifecycle.CONFIGURE_START_EVENT occurs, so this block
+             * must sit after the call to tomcat.start() and it requires
+             * tomcat.enableNaming() to be called much earlier in the code.
+            */
+            if (commandLineParams.enableBasicAuth || commandLineParams.tomcatUsersLocation != null) {
+                configureUserStore(tomcat, commandLineParams)
+            }
+
+            tomcat.server.await()
+        }
+
+        private fun resolveTomcatBaseDir(port: Int?, tempDirectory: String?): String? {
+            val baseDir = if (tempDirectory != null)
+                File(tempDirectory)
+            else
+                File(System.getProperty("user.dir") + "/target/tomcat." + port)
+
+            if (!baseDir.isDirectory)
+                Files.createDirectories(baseDir.toPath())
+
+            return try {
+                baseDir.canonicalPath
+            } catch (e: IOException) {
+                baseDir.absolutePath
+            }
+        }
+
+        private fun enableBasicAuth(ctx: Context, enableSSL: Boolean) {
+            val loginConfig = LoginConfig()
+            loginConfig.authMethod = "BASIC"
+            ctx.loginConfig = loginConfig
+            ctx.addSecurityRole(authRole)
+
+            val securityConstraint = SecurityConstraint()
+            securityConstraint.addAuthRole(authRole)
+            if (enableSSL) {
+                securityConstraint.userConstraint = ServletSecurity.TransportGuarantee.CONFIDENTIAL.toString()
+            }
+            val securityCollection = SecurityCollection()
+            securityCollection.addPattern("/*")
+            securityConstraint.addCollection(securityCollection)
+            ctx.addConstraint(securityConstraint)
+        }
+
+        @Throws(Exception::class)
+        private fun configureUserStore(tomcat: Tomcat, commandLineParams: CommandLineParams) {
+            var tomcatUsersLocation = commandLineParams.tomcatUsersLocation
+            if (tomcatUsersLocation ==
+                null
+            ) {
+                tomcatUsersLocation = "../../tomcat-users.xml"
+            }
+
+            val ref = javax.naming.Reference("org.apache.catalina.UserDatabase")
+            ref.add(StringRefAddr("pathname", tomcatUsersLocation))
+            val memoryUserDatabase = MemoryUserDatabaseFactory().getObjectInstance(
+                ref,
+                CompositeName("UserDatabase"), null, null
+            ) as MemoryUserDatabase
+
+            // Add basic auth user
+            if (commandLineParams.basicAuthUser != null && commandLineParams.basicAuthPw != null) {
+
+                memoryUserDatabase.readonly = false
+                val user = memoryUserDatabase.createRole(authRole, authRole)
+                memoryUserDatabase.createUser(
+                    commandLineParams.basicAuthUser,
+                    commandLineParams.basicAuthPw,
+                    commandLineParams.basicAuthUser
+                ).addRole(user)
+                memoryUserDatabase.save()
+
+            } else if (System.getenv("BASIC_AUTH_USER") != null && System.getenv("BASIC_AUTH_PW") != null) {
+
+                memoryUserDatabase.readonly = false
+                val user = memoryUserDatabase.createRole(authRole, authRole)
+                memoryUserDatabase.createUser(
+                    System.getenv("BASIC_AUTH_USER"),
+                    System.getenv("BASIC_AUTH_PW"),
+                    System.getenv("BASIC_AUTH_USER")
+                ).addRole(user)
+                memoryUserDatabase.save()
+            }
+
+            // Register memoryUserDatabase with GlobalNamingContext
+            println("MemoryUserDatabase: $memoryUserDatabase")
+            tomcat.server.globalNamingContext.addToEnvironment("UserDatabase", memoryUserDatabase)
+
+            val ctxRes = org.apache.tomcat.util.descriptor.web.ContextResource()
+            ctxRes.name = "UserDatabase"
+            ctxRes.auth = "Container"
+            ctxRes.type = "org.apache.catalina.UserDatabase"
+            ctxRes.description = "User database that can be updated and saved"
+            ctxRes.setProperty("factory", "org.apache.catalina.users.MemoryUserDatabaseFactory")
+            ctxRes.setProperty("pathname", tomcatUsersLocation)
+            tomcat.server.globalNamingResources.addResource(ctxRes)
+            tomcat.engine.realm = org.apache.catalina.realm.UserDatabaseRealm()
+        }
+
+        private fun addShutdownHook(tomcat: Tomcat?) {
+
+            // add shutdown hook to stop server
+            Runtime.getRuntime().addShutdownHook(object : Thread() {
+                override fun run() {
+                    try {
+                        tomcat?.server?.stop()
+                    } catch (exception: LifecycleException) {
+                        throw RuntimeException("WARNING: Cannot Stop Tomcat " + exception.message, exception)
+                    }
+
+                }
+            })
+        }
+
+        /**
+         * Export a resource embedded into a Jar file to the local file path.
+         *
+         * @param resourceName ie.: "/SmartLibrary.dll"
+         * @return The path to the exported resource
+         * @throws Exception
+         */
+        private fun exportResource(resourceName: String): File {
+            var stream: InputStream? = null
+            var resStreamOut: OutputStream? = null
+            val jarFolder: String
+            try {
+                stream = Main::class.java.getResourceAsStream(resourceName)
+                if (stream == null)
+                    throw Exception("Cannot get resource \"$resourceName\" from Jar file.")
+
+                jarFolder = File(Main::class.java.protectionDomain.codeSource.location.toURI().path).parentFile
+                    .path.replace('\\', '/')
+                resStreamOut = FileOutputStream(jarFolder + resourceName)
+
+                IOUtils.copy(stream, resStreamOut)
+            } catch (ex: Exception) {
+                throw ex
+            } finally {
+                stream!!.close()
+                resStreamOut!!.close()
+            }
+
+            return File(jarFolder + resourceName)
+        }
+    }
+}
