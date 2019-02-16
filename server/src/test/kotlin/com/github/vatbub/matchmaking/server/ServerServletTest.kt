dedc7d156df25b4b9eb6ba4e60f6f74f86a1654c
Frederik Kammel <vatbub123+github@googlemail.com>
2019-02-16 03:15:03 +0100

    Fixed the formatting and the position of the license header in the ServerServletTest class

--- server/src/test/kotlin/com/github/vatbub/matchmaking/server/ServerServletTest.kt
+++ server/src/test/kotlin/com/github/vatbub/matchmaking/server/ServerServletTest.kt
@@ -1,3 +1,22 @@
+/*-
+ * #%L
+ * matchmaking.standalone-server-launcher
+ * %%
+ * Copyright (C) 2016 - 2019 Frederik Kammel
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.github.vatbub.matchmaking.server
 
 import com.github.vatbub.matchmaking.common.Response
@@ -19,26 +38,6 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.net.URL
-
-/*-
- * #%L
- * matchmaking.server
- * %%
- * Copyright (C) 2016 - 2018 Frederik Kammel
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
 
 class ServerServletTest : KotlinTestSuperclass() {
 
@@ -81,7 +80,7 @@ class ServerServletTest : KotlinTestSuperclass() {
                     for (contentTypePart in contentTypeParts)
                         if (contentTypePart.startsWith("charset="))
                             encoding =
-                                    contentTypePart.split("=".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
+                                contentTypePart.split("=".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
                 }
 
                 if (encoding == null)
