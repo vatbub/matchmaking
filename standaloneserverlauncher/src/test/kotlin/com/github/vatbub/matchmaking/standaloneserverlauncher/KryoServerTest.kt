da9707e704683502c3f02c03ae331a136b13398b
vatbub <vatbub123+github@googlemail.com>
2019-05-11 01:28:50 +0200

    [skip ci] updated the license headers

--- standaloneserverlauncher/src/test/kotlin/com/github/vatbub/matchmaking/standaloneserverlauncher/KryoServerTest.kt
+++ standaloneserverlauncher/src/test/kotlin/com/github/vatbub/matchmaking/standaloneserverlauncher/KryoServerTest.kt
@@ -1,3 +1,22 @@
+/*-
+ * #%L
+ * matchmaking.standalone-server-launcher
+ * %%
+ * Copyright (C) 2016 - 2019 Frederik Kammel
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.github.vatbub.matchmaking.standaloneserverlauncher
 
 import com.esotericsoftware.kryonet.Connection
@@ -123,8 +142,8 @@ open class KryoServerTest : KotlinTestSuperclassWithExceptionHandlerForMultithre
         serverContext.messageDispatcher.registerHandler(handler)
 
         setup.doRequest(DummyRequest(TestUtils.defaultConnectionId, TestUtils.defaultPassword)) {
-                    assertExceptionResponse(InternalServerErrorException(), expectedInnerException, 500, it)
-                }
+            assertExceptionResponse(InternalServerErrorException(), expectedInnerException, 500, it)
+        }
     }
 
     private fun assertExceptionResponse(
