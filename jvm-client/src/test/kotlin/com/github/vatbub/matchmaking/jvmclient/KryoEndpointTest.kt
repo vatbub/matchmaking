a169819a86b1e222c1a87d2718bb2c2b66a39aee
vatbub <vatbub123+github@googlemail.com>
2019-09-14 20:13:04 +0300

    Attempt to fix kryoNet

--- jvm-client/src/test/kotlin/com/github/vatbub/matchmaking/jvmclient/KryoEndpointTest.kt
+++ jvm-client/src/test/kotlin/com/github/vatbub/matchmaking/jvmclient/KryoEndpointTest.kt
@@ -19,33 +19,52 @@
  */
 package com.github.vatbub.matchmaking.jvmclient
 
-import com.esotericsoftware.kryonet.Connection
-import com.esotericsoftware.kryonet.FrameworkMessage
-import com.esotericsoftware.kryonet.Listener
-import com.esotericsoftware.kryonet.Server
+import com.esotericsoftware.kryonet.*
+import com.esotericsoftware.kryonet.Client
 import com.github.vatbub.matchmaking.common.*
+import com.github.vatbub.matchmaking.common.testing.dummies.DummyRequest
+import com.github.vatbub.matchmaking.common.testing.dummies.DummyResponse
+import org.awaitility.Awaitility.await
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import java.util.concurrent.TimeUnit
 
 private class DummyKryoServer : DummyServer<EndpointConfiguration.KryoEndpointConfiguration> {
     val kryoServer = Server()
+    private var disposed = false
+    private object Lock
     override lateinit var dummyMessageGenerator: (Request) -> Response
     override val endpointConfiguration: EndpointConfiguration.KryoEndpointConfiguration
-        get() = EndpointConfiguration.KryoEndpointConfiguration("localhost")
+        get() = EndpointConfiguration.KryoEndpointConfiguration("localhost", KryoCommon.defaultTcpPort, KryoCommon.defaultTcpPort + 50, 99999999)
 
     override fun start() {
-        kryoServer.kryo.registerClasses()
+        synchronized(Lock){
+            if (disposed) throw IllegalStateException("Server already disposed, instance thus cannot be used again")
+        }
         val configuration = endpointConfiguration
         val udpPort = configuration.udpPort
         kryoServer.start()
+        kryoServer.kryo.registerClasses()
         if (udpPort == null)
             kryoServer.bind(configuration.tcpPort)
         else
             kryoServer.bind(configuration.tcpPort, udpPort)
+        logger.info("Server bound")
 
 
         kryoServer.addListener(object : Listener() {
+            override fun connected(connection: Connection?) {
+                logger.info("Server: A client connected to me (${this@DummyKryoServer})")
+            }
+
+            override fun disconnected(connection: Connection?) {
+                logger.info("Server: Someone disconnected from me")
+            }
+
             override fun received(connection: Connection?, receivedObject: Any?) {
+                synchronized(Lock){
+                    if (disposed) throw IllegalStateException("Server already disposed, instance thus cannot be used again")
+                }
                 logger.info("Dummy server: Received: $receivedObject")
                 if (receivedObject is FrameworkMessage.KeepAlive) return
                 receivedObject as Request
@@ -60,11 +79,16 @@ private class DummyKryoServer : DummyServer<EndpointConfiguration.KryoEndpointCo
     }
 
     override fun stop() {
-        kryoServer.stop()
+        synchronized(Lock){
+            disposed = true
+            kryoServer.stop()
+        }
     }
 }
 
-class KryoEndpointTest : ClientEndpointTest<ClientEndpoint.KryoEndpoint, EndpointConfiguration.KryoEndpointConfiguration>(DummyKryoServer()) {
+class KryoEndpointTest : ClientEndpointTest<ClientEndpoint.KryoEndpoint, EndpointConfiguration.KryoEndpointConfiguration>() {
+    override fun newDummyServer(): DummyServer<EndpointConfiguration.KryoEndpointConfiguration> = DummyKryoServer()
+
     override fun newObjectUnderTest(endpointConfiguration: EndpointConfiguration.KryoEndpointConfiguration) =
             ClientEndpoint.KryoEndpoint(endpointConfiguration)
 
@@ -78,5 +102,119 @@ class KryoEndpointTest : ClientEndpointTest<ClientEndpoint.KryoEndpoint, Endpoin
     @Test
     override fun notEqualsTest() {
         Assertions.assertNotEquals(newObjectUnderTest(), newObjectUnderTest(EndpointConfiguration.KryoEndpointConfiguration("localhost", KryoCommon.defaultTcpPort + 1, KryoCommon.defaultTcpPort + 2, 5003)))
+    }
+
+    @Test
+    fun dummyConnectTest() {
+        val server = Server()
+        val client = Client()
+
+        server.start()
+        client.start()
+
+        server.kryo.registerClasses()
+        client.kryo.registerClasses()
+
+        server.bind(KryoCommon.defaultTcpPort + 10)
+        client.connect(5000, "localhost", KryoCommon.defaultTcpPort + 10)
+
+        await().atMost(5, TimeUnit.SECONDS).until { client.isConnected }
+
+        var serverReceived = false
+        var clientReceived = false
+
+        server.addListener(object : Listener() {
+            override fun received(connection: Connection?, receivedObject: Any?) {
+                logger.info("Server: Received: $receivedObject")
+                if (receivedObject is FrameworkMessage.KeepAlive) return
+                serverReceived = true
+                connection!!.sendTCP(DummyResponse())
+            }
+        })
+
+        client.addListener(object : Listener() {
+            override fun received(connection: Connection?, receivedObject: Any?) {
+                logger.info("Client: Received: $receivedObject")
+                if (receivedObject is FrameworkMessage.KeepAlive) return
+                clientReceived = true
+            }
+        })
+
+        client.sendTCP(DummyRequest())
+
+        await().atMost(5, TimeUnit.SECONDS).until { serverReceived }
+        await().atMost(5, TimeUnit.SECONDS).until { clientReceived }
+    }
+
+    @Test
+    fun dummyConnectTest2() {
+        val server = Server()
+        val tempEndpoint = ClientEndpoint.KryoEndpoint(EndpointConfiguration.KryoEndpointConfiguration("localhost", KryoCommon.defaultTcpPort + 20))
+
+        server.start()
+
+        server.kryo.registerClasses()
+
+        server.bind(KryoCommon.defaultTcpPort + 20)
+        tempEndpoint.connect()
+
+        await().atMost(5, TimeUnit.SECONDS).until { tempEndpoint.isConnected }
+
+        var serverReceived = false
+        var clientReceived = false
+
+        server.addListener(object : Listener() {
+            override fun received(connection: Connection?, receivedObject: Any?) {
+                logger.info("Server: Received: $receivedObject")
+                if (receivedObject is FrameworkMessage.KeepAlive) return
+                serverReceived = true
+                connection!!.sendTCP(DummyResponse())
+            }
+        })
+
+        tempEndpoint.sendRequest<DummyResponse>(DummyRequest()) {
+            logger.info("Client: Received: $it")
+            clientReceived = true
+        }
+
+        await().atMost(5, TimeUnit.SECONDS).until { serverReceived }
+        await().atMost(5, TimeUnit.SECONDS).until { clientReceived }
+
+        tempEndpoint.terminateConnection()
+        server.stop()
+    }
+
+    @Test
+    fun dummyConnectTest3() {
+        logger.info("Check 1")
+        val dummyServer = prepareServer()
+        logger.info("Check 2")
+        val tempEndpoint = prepareEndpoint(dummyServer)
+        logger.info("Check 3")
+
+        dummyServer.start()
+        logger.info("Check 4")
+        tempEndpoint.connect()
+        logger.info("Check 5")
+
+        await().atMost(5, TimeUnit.SECONDS).until { tempEndpoint.isConnected }
+
+        var serverReceived = false
+        var clientReceived = false
+
+        dummyServer.dummyMessageGenerator = {
+            serverReceived = true
+            DummyResponse()
+        }
+
+        tempEndpoint.sendRequest<DummyResponse>(DummyRequest()) {
+            clientReceived = true
+        }
+
+        await().atMost(5, TimeUnit.SECONDS).until { serverReceived }
+        await().atMost(5, TimeUnit.SECONDS).until { clientReceived }
+
+        tempEndpoint.terminateConnection()
+        dummyServer.stop()
     }
 }
